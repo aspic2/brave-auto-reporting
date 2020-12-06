bdba55926921f05f0d77e88d3a10825cd1677acd
thomp <thomp@protonmail.com>
2020-12-06 12:14:12 -0700

    adding credentials.rb

---
+++ credentials.rb
@@ -0,0 +1,7 @@
+# Fill in these values with your info and save a local copy
+
+access_token = "Your_access_token"
+
+# To generate an access token:
+username = "Your_Username"
+password = "Your_password"
