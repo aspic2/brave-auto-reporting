7eff1cfc11852fde1878ce00f266454ddc4ccfab
thomp <thomp@protonmail.com>
2020-12-06 21:35:37 -0700

    created shell class SheetsApi

---
+++ sheets.rb
@@ -0,0 +1,9 @@
+class SheetsApi
+  # TODO: make this class read from the spreadsheet of live Brave campaigns and store the data in memory
+
+  def initialize(service)
+    @sheet_id = nil
+    @service = service
+  end
+
+end
