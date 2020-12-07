ad81d36cc040da5e41a8839ff9bf3631aa56293f
thomp <thomp@protonmail.com>
2020-12-06 18:45:04 -0700

    reworked credentials as class

--- credentials.rb
+++ credentials.rb
@@ -1,7 +1,13 @@
 # Fill in these values with your info and save a local copy
+class BraveCredentials
 
-access_token = "Your_access_token"
+  def initialize()
+    @access_token = "Your_access_token"
 
-# To generate an access token:
-username = "Your_Username"
-password = "Your_password"
+    # To generate an access token:
+    @username = "your_username"
+    @password = "your_password"
+  end
+
+  attr_reader :access_token
+end
