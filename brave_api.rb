bfb13caee647903e243cda173f9e545f8be44ae8
thomp <thomp@protonmail.com>
2020-12-06 20:07:56 -0700

    moved logic into BraveReportsApi class

--- brave_api.rb
+++ brave_api.rb
@@ -1,23 +1,47 @@
 require './credentials.rb'
 require './campaigns.rb'
 
+require 'uri'
 require 'open-uri'
 
 brave_credentials = BraveCredentials.new()
 campaign = Campaign.new()
 
-headers = {
-    "authorization": "Bearer #{brave_credentials.access_token}",
-    # "content-type" as a string
-    'contenttype': 'application/json',
-}
 
-puts headers
+class BraveReportsApi
 
-url = "https://ads-serve.brave.com/v1/report/campaign/csv/#{campaign.campaign_id}"
+  def initialize(credentials)
+    @credentials = credentials
+    @headers = {
+        "authorization": "Bearer #{credentials.access_token}",
+        # "content-type" as a string
+        'contenttype': 'application/json',
+    }
+    @url = nil
+    @report = nil
+  end
 
-report = URI.open(url, "authorization" => headers.fetch(:authorization), "content-type" => headers.fetch(:contenttype))
+  attr_accessor :url, :report
 
-report.each_line {|line|
-  puts line
-}
+  def build_url(campaign_id)
+    @url = URI.join("https://ads-serve.brave.com/v1/report/campaign/csv/", campaign_id)
+    return self
+  end
+
+  def retrieve_report()
+    @report = URI.open(@url, "authorization" => @headers.fetch(:authorization), "content-type" => @headers.fetch(:contenttype))
+  end
+
+  def print_report()
+    @report.each_line {|line|
+      puts line
+    }
+  end
+
+end
+
+
+if __FILE__ == $0
+  api = BraveReportsApi.new(brave_credentials).build_url(campaign.campaign_id)
+  api.retrieve_report()
+end
