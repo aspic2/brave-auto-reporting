4fbf3ea56d0eb0de875c56708c6d2bd6e8cb291b
thomp <thomp@protonmail.com>
2020-12-07 12:17:56 -0700

    create class Campaigns to store campaign metadata

--- campaigns.rb
+++ campaigns.rb
@@ -1,8 +1,17 @@
+require './test_data.rb'
+
 class Campaign
 
-  def initialize()
-    @campaign_id = "your_campaign_id"
+  def initialize(campaign_hash)
+    @campaign_name = campaign_hash.fetch(:campaign_name)
+    @campaign_id = campaign_hash.fetch(:campaign_id)
+    @sheets_url = campaign_hash.fetch(:sheets_url)
   end
 
-  attr_reader :campaign_id
+  attr_reader :campaign_name, :campaign_id, :sheets_url
+end
+
+if __FILE__ == $0
+  data = TestData.new()
+  campaign = Campaign.new(data.campaign_hash)
 end
