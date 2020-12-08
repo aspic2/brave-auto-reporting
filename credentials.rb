cec9053d2daee7691ef4e0b4cf4e17783cb8a833
thomp <thomp@protonmail.com>
2020-12-07 20:00:50 -0700

    added GoogleCredentials class, and BraveCredentials.report_url

--- credentials.rb
+++ credentials.rb
@@ -7,7 +7,17 @@ class BraveCredentials
     # To generate an access token:
     @username = "your_username"
     @password = "your_password"
+    @report_url = "your_base_url_for_reports"
   end
 
-  attr_reader :access_token
+  attr_reader :access_token, :report_url
+end
+
+class GoogleCredentials
+  def initialize()
+    @reports_list_sheet_id = "your_google_sheet_id_for_your_list_of_reports"
+    @reports_list_range = "Reports List!A2:D"
+  end
+
+  attr_reader :reports_list_sheet_id, :reports_list_range, :write_to_range_without_tab_name
 end
