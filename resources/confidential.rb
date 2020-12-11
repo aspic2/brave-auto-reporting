f8564bb269085659351d09cdfa1e8ec3ae4591f4
thomp <thomp@protonmail.com>
2020-12-11 10:20:35 -0700

    created resources/confidential.rb

---
+++ resources/confidential.rb
@@ -0,0 +1,17 @@
+# Use this file to store things like access tokens, passwords, and
+# campaign IDs that should not be shared in the remote repository.
+
+module Confidential
+  brave_access_token = ""
+  brave_username = ""
+  brave_api_url = ""
+
+  google_reports_list_id = ""
+  google_reports_list_range = ""
+
+  test_campaign_hash = {"campaign_name": "",
+    "campaign_id": "",
+    "sheets_url": "",
+    "tab_name": ""
+  }
+end
